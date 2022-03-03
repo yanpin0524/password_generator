a656a76b07e5b398940b1b6b1d103814eb2cb42e
Leo <a131234568@gmail.com>
2022-03-03 15:23:15 +0800

    產生密碼：步驟一

---
+++ generate_password.js
@@ -0,0 +1,45 @@
+function generatePassword() {
+  // define things user might want
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
+
+  const options = {
+    length: 12,
+    lowercase: 'on',
+    uppercase: 'on',
+    numbers: 'on',
+    excludeCharacters: '40'
+  }
+
+  // create a collection to store things user picked up
+  let collection = []
+
+  if (options.lowercase === 'on') {
+    collection = collection.concat(lowerCaseLetters.split(''))
+  }
+
+  if (options.uppercase === 'on') {
+    collection = collection.concat(upperCaseLetters.split(''))
+  }
+
+  if (options.numbers === 'on') {
+    collection = collection.concat(numbers.split(''))
+  }
+
+  if (options.symbols === 'on') {
+    collection = collection.concat(symbols.split(''))
+  }
+
+  console.log('collection', collection)
+  // remove things user do not need
+
+  // start generating password
+
+  // return the generated password
+  console.log('This function will generate password')
+}
+
+
+generatePassword()
